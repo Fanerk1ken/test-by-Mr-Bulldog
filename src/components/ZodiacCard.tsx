faa09e421c8e40d033c6eb50a8ad20a5981446d2
egornikandrov <egr.nikandrov@gmail.com>
2024-08-23 00:13:15 +0300

    1. Изменил стили

--- src/components/ZodiacCard.tsx
+++ src/components/ZodiacCard.tsx
@@ -9,7 +9,7 @@ interface ZodiacCardProps {
 
 const ZodiacCard: React.FC<ZodiacCardProps> = ({ zodiac, onClick, language }) => (
     <div className="zodiac-card" onClick={onClick}>
-        <img src={`/icons/${zodiac.sign}.svg`} alt={zodiac.sign} className="zodiac-icon" />
+        <img src={`/icons/${zodiac.sign}.svg`} alt={zodiac.sign} className="zodiac-icon" style={{filter: 'invert(1)'}}/>
         <h2>{zodiac[language].name}</h2>
         <p>{zodiac.period}</p>
     </div>
